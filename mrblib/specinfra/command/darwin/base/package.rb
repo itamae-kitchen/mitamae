6627170a3c08653c12ab12333291c043485a2138
Takashi Kokubun <takashi-kokubun@cookpad.com>
2016-08-24 04:49:36 +0900

    Resolve namespace properly for File
    NoMethodError: undefined method 'basename' for
    Specinfra::Command::Darwin::Base::File

--- mrblib/specinfra/command/darwin/base/package.rb
+++ mrblib/specinfra/command/darwin/base/package.rb
@@ -1,7 +1,7 @@
 class Specinfra::Command::Darwin::Base::Package < Specinfra::Command::Base::Package
   class << self
     def check_is_installed(package, version=nil)
-      escaped_package = escape(File.basename(package))
+      escaped_package = escape(::File.basename(package))
       if version
         cmd = %Q[brew info #{escaped_package} | grep -E "^$(brew --prefix)/Cellar/#{escaped_package}/#{escape(version)}"]
       else
@@ -13,7 +13,7 @@ class Specinfra::Command::Darwin::Base::Package < Specinfra::Command::Base::Pack
     alias :check_is_installed_by_homebrew :check_is_installed
 
     def check_is_installed_by_homebrew_cask(package, version=nil)
-      escaped_package = escape(File.basename(package))
+      escaped_package = escape(::File.basename(package))
       if version
         cmd = "brew cask info #{escaped_package} | grep -E '^/opt/homebrew-cask/Caskroom/#{escaped_package}/#{escape(version)}'"
       else
