7b16cf51d6d654a9697596811af16fe4e8680ff1
Takashi Kokubun <takashi-kokubun@cookpad.com>
2016-10-07 18:41:34 +0900

    Avoid inspecting arguments for Regexp.compile
    Experiencing SEGV many times on:
    https://github.com/mattn/mruby-onig-regexp/blob/fdb7f839bca649c6456666355d374bd897c6c2d1/mrblib/onig_regexp.rb#L6
    
    0  mrb_gc_mark (obj=0x20, mrb=0x706010) at /home/k0kubun/src/github.com/k0kubun/itamae-mruby/mruby/src/gc.c:675
    1  gc_mark_children (gc=0x7060e8, obj=<optimized out>, mrb=0x706010) at /home/k0kubun/src/github.com/k0kubun/itamae-mruby/mruby/src/gc.c:623
    2  gc_gray_mark (obj=<optimized out>, gc=0x7060e8, mrb=0x706010) at /home/k0kubun/src/github.com/k0kubun/itamae-mruby/mruby/src/gc.c:833
    3  incremental_marking_phase (limit=<optimized out>, gc=<optimized out>, mrb=<optimized out>)
       at /home/k0kubun/src/github.com/k0kubun/itamae-mruby/mruby/src/gc.c:928
    4  incremental_gc (mrb=mrb@entry=0x706010, gc=gc@entry=0x7060e8, limit=limit@entry=2000)
       at /home/k0kubun/src/github.com/k0kubun/itamae-mruby/mruby/src/gc.c:1027
    5  0x00000000004328ec in incremental_gc (limit=<optimized out>, gc=<optimized out>, mrb=<optimized out>)
       at /home/k0kubun/src/github.com/k0kubun/itamae-mruby/mruby/src/gc.c:1019
    6  incremental_gc_step (gc=0x7060e8, mrb=0x706010) at /home/k0kubun/src/github.com/k0kubun/itamae-mruby/mruby/src/gc.c:1062
    7  mrb_incremental_gc (mrb=0x706010) at /home/k0kubun/src/github.com/k0kubun/itamae-mruby/mruby/src/gc.c:1106
    8  0x0000000000433065 in mrb_obj_alloc (mrb=mrb@entry=0x706010, ttype=ttype@entry=MRB_TT_ARRAY, cls=0x70fd50)
       at /home/k0kubun/src/github.com/k0kubun/itamae-mruby/mruby/src/gc.c:486
    9  0x000000000041bebd in ary_new_capa (capa=0, mrb=0x706010) at /home/k0kubun/src/github.com/k0kubun/itamae-mruby/mruby/src/array.c:42
    10 mrb_ary_new_from_values (mrb=mrb@entry=0x706010, size=size@entry=0, vals=vals@entry=0x8a71d0)
       at /home/k0kubun/src/github.com/k0kubun/itamae-mruby/mruby/src/array.c:90
    11 0x00000000004045cb in mrb_vm_exec (mrb=mrb@entry=0x706010, proc=<optimized out>, proc@entry=0x803f20, pc=<optimized out>)
       at /home/k0kubun/src/github.com/k0kubun/itamae-mruby/mruby/src/vm.c:2115
    12 0x0000000000408171 in mrb_vm_run (mrb=0x706010, proc=0x803f20, self=..., stack_keep=<optimized out>)
       at /home/k0kubun/src/github.com/k0kubun/itamae-mruby/mruby/src/vm.c:759
    13 0x0000000000408635 in mrb_run (self=..., proc=0x803f20, mrb=0x706010) at /home/k0kubun/src/github.com/k0kubun/itamae-mruby/mruby/src/vm.c:2424
    14 mrb_funcall_with_block (mrb=mrb@entry=0x706010, self=..., mid=<optimized out>, mid@entry=1435, argc=<optimized out>, argc@entry=1,
       argv=argv@entry=0x7fffffffe6b0, blk=...) at /home/k0kubun/src/github.com/k0kubun/itamae-mruby/mruby/src/vm.c:414
    15 0x00000000004089fd in mrb_funcall_with_block (mrb=mrb@entry=0x706010, self=..., mid=1435, argc=argc@entry=1, argv=argv@entry=0x7fffffffe6b0, blk=...)
       at /home/k0kubun/src/github.com/k0kubun/itamae-mruby/mruby/src/vm.c:334
    16 0x0000000000408de2 in mrb_funcall_argv (argv=0x7fffffffe6b0, argc=1, mid=<optimized out>, self=..., mrb=0x706010)
       at /home/k0kubun/src/github.com/k0kubun/itamae-mruby/mruby/src/vm.c:424
    17 mrb_funcall (mrb=mrb@entry=0x706010, self=..., name=name@entry=0x46f009 "__main__", argc=argc@entry=1)
       at /home/k0kubun/src/github.com/k0kubun/itamae-mruby/mruby/src/vm.c:319
    18 0x0000000000402ee1 in main (argc=<optimized out>, argv=<optimized out>) at /home/k0kubun/src/github.com/k0kubun/itamae-mruby/tools/itamae/itamae.c:22

--- mrblib/specinfra/string_utils.rb
+++ mrblib/specinfra/string_utils.rb
@@ -2,9 +2,12 @@
 #   https://github.com/mizzy/specinfra/blob/v2.60.2/lib/specinfra/ext/string.rb
 module Specinfra
   module StringUtils
+    UNDERSCORE_REGEXP = /_/
+    CAMEL_CASE_REGEXP = /[A-Z]+.*/
+
     class << self
       def to_camel_case(str)
-        return str if str !~ /_/ && str =~ /[A-Z]+.*/
+        return str if str !~ UNDERSCORE_REGEXP && str =~ CAMEL_CASE_REGEXP
         str.split('_').map { |e| e.capitalize }.join
       end
     end
