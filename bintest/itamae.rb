731c6e201d7acb5491827fb1bdee448eef728518
Takashi Kokubun <takashikkbn@gmail.com>
2016-10-30 16:51:36 +0900

    Fix bintest to be run

--- bintest/itamae.rb
+++ bintest/itamae.rb
@@ -2,16 +2,15 @@ require 'open3'
 
 BIN_PATH = File.join(File.dirname(__FILE__), "../mruby/bin/itamae")
 
-assert('hello') do
+assert('itamae') do
   output, status = Open3.capture2(BIN_PATH)
-
   assert_true status.success?, "Process did not exit cleanly"
-  assert_include output, "Hello World"
 end
 
-assert('version') do
+assert('itamae version') do
   output, status = Open3.capture2(BIN_PATH, "version")
-
   assert_true status.success?, "Process did not exit cleanly"
-  assert_include output, "v0.0.1"
+
+  require_relative '../mrblib/itamae/version'
+  assert_include output, Itamae::VERSION
 end
