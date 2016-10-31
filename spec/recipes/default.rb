161b8536517a6869f699fdb401f64b74fc31f44b
Takashi Kokubun <takashi-kokubun@cookpad.com>
2016-10-31 14:12:03 +0900

    Test directory resource

--- spec/recipes/default.rb
+++ spec/recipes/default.rb
@@ -1,11 +1,12 @@
 include_recipe 'included.rb'
 include_recipe 'included.rb' # should be skipped
 
+include_recipe 'user'
+include_recipe 'directory'
 include_recipe 'execute'
 include_recipe 'file'
 include_recipe 'notifies'
 include_recipe 'remote_file'
-include_recipe 'user'
 
 # Slow recipes are executed separately. See Dockerfile.
 # include_recipe 'package'
