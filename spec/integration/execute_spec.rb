7edc3587b29c3b9746edcfbea1dd42950e2fc7cf
Takashi Kokubun <takashi-kokubun@cookpad.com>
2016-10-31 14:14:26 +0900

    Add file resource tests

--- spec/integration/execute_spec.rb
+++ spec/integration/execute_spec.rb
@@ -4,3 +4,11 @@ describe file('/tmp/execute') do
   it { should be_file }
   its(:content) { should match(/Hello Execute/) }
 end
+
+describe file('/tmp/never_exist1') do
+  it { should_not be_file }
+end
+
+describe file('/tmp/never_exist2') do
+  it { should_not be_file }
+end
--- spec/recipes/file.rb
+++ spec/recipes/file.rb
@@ -2,3 +2,11 @@ file "/tmp/file" do
   content "Hello World"
   mode "777"
 end
+
+file "/tmp/never_exist1" do
+  only_if "exit 1"
+end
+
+file "/tmp/never_exist2" do
+  not_if "exit 0"
+end
