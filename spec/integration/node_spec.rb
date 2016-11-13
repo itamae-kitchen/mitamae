45401160d19e8738d73d474c9d83ec2ab572a182
Takashi Kokubun <takashikkbn@gmail.com>
2016-11-13 18:49:50 -0500

    Test node.yml

--- spec/integration/node_spec.rb
+++ spec/integration/node_spec.rb
@@ -2,11 +2,16 @@ require 'spec_helper'
 
 describe 'node object' do
   before(:all) do
-    apply_recipe('node', options: %w[-j /recipes/node.json])
+    apply_recipe('node', options: %w[-j /recipes/node.json -y /recipes/node.yml])
   end
 
   describe file('/tmp/node_json') do
     it { should be_file }
     its(:content) { should eq('node.json') }
+  end
+
+  describe file('/tmp/node_yml') do
+    it { should be_file }
+    its(:content) { should eq('node.yml') }
   end
 end
--- spec/recipes/node.rb
+++ spec/recipes/node.rb
@@ -1,3 +1,7 @@
 file '/tmp/node_json' do
   content node[:node_json]
 end
+
+file '/tmp/node_yaml' do
+  content node[:node_yml]
+end
