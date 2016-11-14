ef8d8d17dee38e844d0b9243dce7a0bbaa9b33e7
Takashi Kokubun <takashikkbn@gmail.com>
2016-11-14 14:09:26 -0600

    Pass link spec

--- spec/integration/link_spec.rb
+++ spec/integration/link_spec.rb
@@ -1,12 +1,18 @@
 require 'spec_helper'
 
-describe file('/tmp-link') do
-  it { should be_linked_to '/tmp' }
-  its(:content) do
-    expect(subject.content.lines.size).to eq 0
+describe 'link resource' do
+  before(:all) do
+    apply_recipe('link')
   end
-end
 
-describe file('/tmp-link-force') do
-  it { should be_linked_to '/tmp' }
+  describe file('/tmp-link') do
+    it { should be_linked_to '/tmp' }
+    its(:content) do
+      expect(subject.content.lines.size).to eq 0
+    end
+  end
+
+  describe file('/tmp-link-force') do
+    it { should be_linked_to '/tmp' }
+  end
 end
--- spec/recipes/default.rb
+++ spec/recipes/default.rb
@@ -1,4 +1,3 @@
-include_recipe 'link'
 include_recipe 'notifies'
 include_recipe 'remote_file'
 include_recipe 'run_command'
