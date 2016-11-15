32085f320561a71d28a344a06654ea1d72031465
Takashi Kokubun <takashikkbn@gmail.com>
2016-11-16 05:07:22 +0900

    Mount host directory to run serverspec
    Regression of d857b8b

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -33,7 +33,7 @@ RSpec.configure do |config|
     system('docker', 'rm', '-f', MItamaeSpec.container)
     system(
       'docker', 'run', '-d', '--name', MItamaeSpec.container,
-      '-v', "#{File.expand_path('mruby/build/x86_64-pc-linux-gnu')}:/mitamae",
+      '-v', "#{File.expand_path('mruby/build/host')}:/mitamae",
       '-v', "#{File.expand_path('spec/recipes')}:/recipes",
       'ubuntu:trusty', 'bash', '-c', 'while true; do sleep 3600; done',
     ) || raise
