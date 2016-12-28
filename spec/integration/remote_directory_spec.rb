26be8e3f2f91264c8954dab52f2c5e0acc58bb3f
Shimpei Makimoto <makimoto@tsuyabu.in>
2016-12-28 23:03:01 +0900

    Add spec example for 90efc67b3

--- spec/integration/remote_directory_spec.rb
+++ spec/integration/remote_directory_spec.rb
@@ -13,4 +13,20 @@ describe 'remote_directory resource' do
     it { should be_file }
     its(:content) { should eq("Hello\n") }
   end
+
+  context 'when desired directory exists' do
+    before(:all) do
+      apply_recipe('remote_directory')
+      apply_recipe('remote_directory') # Do twice
+    end
+
+    describe file('/tmp/remdir') do
+      it { should be_directory }
+    end
+  
+    describe file('/tmp/remdir/file') do
+      it { should be_file }
+      its(:content) { should eq("Hello\n") }
+    end
+  end
 end
