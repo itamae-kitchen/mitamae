204c8e82d93dabe5c1b8a46201489c13fb778545
Civitaspo <civitaspo@gmail.com>
2017-02-21 11:23:18 +0900

    Support force link to a direcotory (#54)

--- spec/recipes/link.rb
+++ spec/recipes/link.rb
@@ -7,3 +7,18 @@ link "/tmp-link-force" do
   to "/tmp"
   force true
 end
+
+execute "mkdir /tmp/link-force-no-dereference1"
+link "link-force-no-dereference" do
+  cwd "/tmp"
+  to "link-force-no-dereference1"
+  force true
+end
+
+execute "mkdir /tmp/link-force-no-dereference2"
+link "link-force-no-dereference" do
+  cwd "/tmp"
+  to "link-force-no-dereference2"
+  force true
+end
+
