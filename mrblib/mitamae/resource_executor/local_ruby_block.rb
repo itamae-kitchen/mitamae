58810f4e4c0b1b131353f3f5e733920fe969a2ad
Kohei Suzuki <github@wanko.cc>
2017-02-21 11:46:46 +0900

    Notify local_ruby_block changes (#56)

--- mrblib/mitamae/resource_executor/local_ruby_block.rb
+++ mrblib/mitamae/resource_executor/local_ruby_block.rb
@@ -2,17 +2,25 @@ module MItamae
   module ResourceExecutor
     class LocalRubyBlock < Base
       def apply
-        desired.block.call
+        if desired.executed
+          desired.block.call
+        end
       end
 
       private
 
-      def set_current_attributes(*)
-        # noop
+      def set_current_attributes(current, action)
+        case action
+        when :run
+          current.executed = false
+        end
       end
 
-      def set_desired_attributes(*)
-        # noop
+      def set_desired_attributes(desired, action)
+        case action
+        when :run
+          desired.executed = true
+        end
       end
     end
   end
