16cdfcbf2060a47c4936568acd70f2221b4b5484
Takashi Kokubun <takashikkbn@gmail.com>
2017-05-02 15:12:29 -0700

    Drop invalid group specification

--- release.sh
+++ release.sh
@@ -2,5 +2,5 @@
 # This script is created because `rake release` doesn't work somehow...
 
 sudo docker-compose run -e BUILD_TARGET=all compile
-sudo chown -R "${USER}:${USER}" mruby/build
+sudo chown -R "${USER}" mruby/build
 rake release
