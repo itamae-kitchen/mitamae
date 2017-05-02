af7833356c56af104a7f87111855ad7ed7ddaf86
Takashi Kokubun <takashikkbn@gmail.com>
2017-05-02 15:13:28 -0700

    Initialize submodule automatically

--- release.sh
+++ release.sh
@@ -1,6 +1,10 @@
 #!/bin/bash -ex
 # This script is created because `rake release` doesn't work somehow...
 
+if [ ! -f mruby/Rakefile ]; then
+  git submodule init
+  git submodule update
+fi
 sudo docker-compose run -e BUILD_TARGET=all compile
 sudo chown -R "${USER}" mruby/build
 rake release
