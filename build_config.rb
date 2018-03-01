8dd06d6a565964f5c75331485f158367e1bf135e
Takashi Kokubun <takashikkbn@gmail.com>
2018-03-01 14:20:49 +0900

    Upgrade mruby-specinfra to allow Symbol in EC2
    metadata host inventory. This is special extension for mitamae which is
    not in original specinfra. But I think it's safe to have interface
    comaptible with other inventories.

--- build_config.rb
+++ build_config.rb
@@ -9,7 +9,7 @@ def gem_config(conf)
   conf.gem mgem: 'mruby-io',             checksum_hash: '6836f424c5ff95d0114a426010b22254804bc9a3'
   conf.gem mgem: 'mruby-open3',          checksum_hash: 'b7480b6300a81d0e5fac469a36a383518e3dfc78'
   conf.gem mgem: 'mruby-shellwords',     checksum_hash: '2a284d99b2121615e43d6accdb0e4cde1868a0d8'
-  conf.gem mgem: 'mruby-specinfra',      checksum_hash: '2ae9d7afe8fc24df2378de5668acf5f1faeeb6c6'
+  conf.gem mgem: 'mruby-specinfra',      checksum_hash: 'd802a755cfa94675c6df80547ca553abb323ec7f'
   conf.gem github: 'k0kubun/mruby-erb',  checksum_hash: '978257e478633542c440c9248e8cdf33c5ad2074'
   conf.gem github: 'eagletmt/mruby-etc', checksum_hash: 'v0.1.0'
   conf.gem github: 'take-cheeze/mruby-socket', checksum_hash: '7b4978b639e9b73296f5ced39dbcb722bf42cd0d' # https://github.com/iij/mruby-socket/issues/36
