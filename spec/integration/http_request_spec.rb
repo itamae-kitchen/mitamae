4a73d8cb7cc2d25031cdfbd2354f66124dcfaeb7
Takashi Kokubun <takashikkbn@gmail.com>
2018-05-09 22:02:33 +0900

    Match more ambiguously to adapt httpbin changes

--- spec/integration/http_request_spec.rb
+++ spec/integration/http_request_spec.rb
@@ -7,33 +7,33 @@ describe 'http_request resource' do
 
   describe file('/tmp/http_request.html') do
     it { should be_file }
-    its(:content) { should match(/"from": "itamae"/) }
+    its(:content) { should match(/"from": ?"itamae"/) }
   end
 
   describe file('/tmp/http_request_delete.html') do
     it { should be_file }
-    its(:content) { should match(/"from": "itamae"/) }
+    its(:content) { should match(/"from": ?"itamae"/) }
   end
 
   describe file('/tmp/http_request_post.html') do
     it { should be_file }
     its(:content) do
-      should match(/"from": "itamae"/)
-      should match(/"data": "love=sushi"/)
+      should match(/"from": ?"itamae"/)
+      should match(/"data": ?"love=sushi"/)
     end
   end
 
   describe file('/tmp/http_request_put.html') do
     it { should be_file }
     its(:content) do
-      should match(/"from": "itamae"/)
-      should match(/"data": "love=sushi"/)
+      should match(/"from": ?"itamae"/)
+      should match(/"data": ?"love=sushi"/)
     end
   end
 
   describe file('/tmp/http_request_headers.html') do
     it { should be_file }
-    its(:content) { should match(/"User-Agent": "Itamae"/) }
+    its(:content) { should match(/"User-Agent": ?"Itamae"/) }
   end
 
   # describe file('/tmp/http_request_redirect.html') do
