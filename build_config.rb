d1905c5e02a44c72da5b7b8516fcb55c3bf955db
Takashi Kokubun <takashikkbn@gmail.com>
2019-02-11 15:55:17 +0900

    Upgrade mruby-specinfra
    to merge https://github.com/itamae-kitchen/mruby-specinfra/pull/9

--- build_config.rb
+++ build_config.rb
@@ -9,7 +9,7 @@ def gem_config(conf)
   conf.gem mgem: 'mruby-io',             checksum_hash: '6836f424c5ff95d0114a426010b22254804bc9a3'
   conf.gem mgem: 'mruby-open3',          checksum_hash: 'b7480b6300a81d0e5fac469a36a383518e3dfc78'
   conf.gem mgem: 'mruby-shellwords',     checksum_hash: '2a284d99b2121615e43d6accdb0e4cde1868a0d8'
-  conf.gem mgem: 'mruby-specinfra',      checksum_hash: '607e0380fd3aaa96f75feae7d09c94dc9784b728'
+  conf.gem mgem: 'mruby-specinfra',      checksum_hash: 'efb18feeee0cb8d0d49c7a9eadbc0fc074c2be1c'
   conf.gem mgem: 'mruby-socket',         checksum_hash: 'a8b6d6ee4c6ccea81a805cc9204b36c3792123c9'
   conf.gem github: 'k0kubun/mruby-erb',  checksum_hash: '978257e478633542c440c9248e8cdf33c5ad2074'
   conf.gem github: 'eagletmt/mruby-etc', checksum_hash: 'v0.1.0'
