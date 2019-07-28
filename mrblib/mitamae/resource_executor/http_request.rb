c5b2e90708c90574c5208a5abb685a543c8e5231
takumin <takumiiinn@gmail.com>
2019-07-29 01:38:30 +0900

    Changed http_request resource to Itamae compatible
    - Remove check_error attribute:
      https://github.com/itamae-kitchen/mitamae/commit/27ad78fbb69707514a1a65a410a594be23ec595d
    - Check HTTP status code:
      https://github.com/itamae-kitchen/itamae/commit/28b359d6c7a4487e1562396dfefc31c1fafd42bd

--- mrblib/mitamae/resource_executor/http_request.rb
+++ mrblib/mitamae/resource_executor/http_request.rb
@@ -1,6 +1,11 @@
 module MItamae
   module ResourceExecutor
     class HTTPRequest < File
+      RedirectLimitExceeded = Class.new(StandardError)
+      HTTPClientError = Class.new(StandardError)
+      HTTPServerError = Class.new(StandardError)
+      HTTPUnknownError = Class.new(StandardError)
+
       HTTP_METHODS = [
         :get,
         :post,
@@ -20,15 +25,15 @@ module MItamae
           raise "invalid http_request action: #{desired.action.inspect}"
         end
 
-        curl(desired.action.to_s.upcase, desired.url, desired.message, desired.headers, desired.check_error).stdout
+        curl(desired.action.to_s.upcase, desired.url, desired.message, desired.headers, desired.redirect_limit).stdout
       end
 
-      def curl(method, url, body, headers, check_error)
+      def curl(method, url, body, headers, redirect_limit)
         if run_command(['which', 'curl'], error: false).exit_status != 0
           raise "`curl` command is not available. Please install curl to use mitamae's http_request."
         end
 
-        command = ['curl', '-fsSL', '-X', method, url]
+        command = ['curl', '-fsSL', '--max-redirs', "#{redirect_limit}", '-X', method, url]
         unless body.empty?
           command.concat(['-d', body])
         end
@@ -37,8 +42,20 @@ module MItamae
         end
 
         result = run_command(command, error: false)
-        if check_error and result.exit_status != 0
-          raise "failed to execute curl: #{result.stderr}"
+        unless result.success?
+          MItamae.logger.error("#{result.stderr}")
+          case result.exit_status
+          when 22
+            case result.stderr.gsub(/\Acurl:.*:\s(\d{3}.*)\z/, '\1')
+            when /\A4\d{2}/
+              raise HTTPClientError
+            when /\A5\d{2}/
+              raise HTTPServerError
+            end
+          when 47
+            raise RedirectLimitExceeded
+          end
+          raise HTTPUnknownError
         end
         result
       end
