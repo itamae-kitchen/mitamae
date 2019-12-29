498c4e217513cf41930eb4af86988f2e53960f10
Takashi Kokubun <takashikkbn@gmail.com>
2019-12-29 11:22:05 -0800

    Upgrade mruby-specinfra
    to https://github.com/itamae-kitchen/mruby-specinfra/commit/60d100973f085381e291fb4453a7a628f0d03cb5

--- build_config.rb
+++ build_config.rb
@@ -8,7 +8,7 @@ def gem_config(conf)
   conf.gem mgem: 'mruby-hashie',         checksum_hash: 'c69255a94debcd641f2087b569f5625509bde698'
   conf.gem mgem: 'mruby-open3',          checksum_hash: 'b7480b6300a81d0e5fac469a36a383518e3dfc78'
   conf.gem mgem: 'mruby-shellwords',     checksum_hash: '2a284d99b2121615e43d6accdb0e4cde1868a0d8'
-  conf.gem mgem: 'mruby-specinfra',      checksum_hash: 'be5e6fe1c575106308ba49236c802a1ade925e45'
+  conf.gem mgem: 'mruby-specinfra',      checksum_hash: '60d100973f085381e291fb4453a7a628f0d03cb5'
   conf.gem github: 'k0kubun/mruby-erb',  checksum_hash: '978257e478633542c440c9248e8cdf33c5ad2074'
   conf.gem github: 'mrbgems/mruby-yaml', checksum_hash: '94f429717fd234767a15186beacdc50ae582463c'
   conf.gem github: 'eagletmt/mruby-etc', checksum_hash: 'v0.1.0'
