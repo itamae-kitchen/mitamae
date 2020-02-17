d087445ca0164ec487a249392f2cc03027c2aad1
Takashi Kokubun <takashikkbn@gmail.com>
2020-02-17 12:32:14 -0800

    Upgrade specinfra to v2.82.9
    https://github.com/mizzy/specinfra/compare/v2.82.8...v2.82.9
    https://github.com/itamae-kitchen/mruby-specinfra/compare/d67d734...95994e4

--- build_config.rb
+++ build_config.rb
@@ -8,7 +8,7 @@ def gem_config(conf)
   conf.gem mgem: 'mruby-hashie',             checksum_hash: 'c69255a94debcd641f2087b569f5625509bde698'
   conf.gem mgem: 'mruby-open3',              checksum_hash: 'b7480b6300a81d0e5fac469a36a383518e3dfc78'
   conf.gem mgem: 'mruby-shellwords',         checksum_hash: '2a284d99b2121615e43d6accdb0e4cde1868a0d8'
-  conf.gem mgem: 'mruby-specinfra',          checksum_hash: 'd67d7344aa8129b097e729ce5474afe77c38ac51'
+  conf.gem mgem: 'mruby-specinfra',          checksum_hash: '95994e41b5d40f50dbf0e10ff20a38bd124fd30c'
   conf.gem github: 'k0kubun/mruby-erb',      checksum_hash: '978257e478633542c440c9248e8cdf33c5ad2074'
   conf.gem github: 'k0kubun/mruby-tempfile', checksum_hash: 'e628c8fcb4bca3f3456640a8b56d1ae98c594e24'
   conf.gem github: 'mrbgems/mruby-yaml',     checksum_hash: '94f429717fd234767a15186beacdc50ae582463c'
