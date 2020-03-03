e08a9d9ee946f29244f323d184317b21df325b6a
Takashi Kokubun <takashikkbn@gmail.com>
2020-03-02 21:11:58 -0800

    Make virtualization optional for Actions

--- spec/integration/host_inventory_spec.rb
+++ spec/integration/host_inventory_spec.rb
@@ -15,7 +15,7 @@ describe 'host_inventory' do
     platform_version: /\A14.04\z/,
     filesystem: /"kb_size"/,
     cpu: /"cpu_family"/,
-    virtualization: /\A{"system"=>"docker"}\z/,
+    virtualization: /\A{("system"=>"docker")?}\z/,
     kernel: /"name"=>"Linux"/,
     block_device: /\A{}\z/,
     user: /"root"=>{[^{}]*"uid"=>"0", /,
