494d611cced650e6d92a5f95d44a94e27f565741
Takashi Kokubun <takashikkbn@gmail.com>
2020-03-05 23:40:02 -0800

    Fork mruby-open3 and mruby-process

--- build_config.rb
+++ build_config.rb
@@ -5,11 +5,12 @@ def gem_config(conf)
   conf.gem File.expand_path(File.dirname(__FILE__))
 
   conf.gem mgem: 'mruby-hashie',                checksum_hash: 'c69255a94debcd641f2087b569f5625509bde698'
-  conf.gem mgem: 'mruby-open3',                 checksum_hash: 'b7480b6300a81d0e5fac469a36a383518e3dfc78'
+  conf.gem mgem: 'mruby-open3',                 checksum_hash: '5608d98f52303072af0e5ee26a45852e2720ce19'
   conf.gem mgem: 'mruby-shellwords',            checksum_hash: '2a284d99b2121615e43d6accdb0e4cde1868a0d8'
   conf.gem mgem: 'mruby-specinfra',             checksum_hash: '6caf46e69238b50054bed78cb86ead49c3b7730c'
   conf.gem github: 'k0kubun/mruby-erb',         checksum_hash: '978257e478633542c440c9248e8cdf33c5ad2074'
   conf.gem github: 'k0kubun/mruby-file-stat',   checksum_hash: '13297c3e60d93b0ce763260b9047cac375ea7f12'
+  conf.gem github: 'k0kubun/mruby-process',     checksum_hash: 'd908a208ce6e4b89b9405f63c3580664008fbb0f'
   conf.gem github: 'k0kubun/mruby-tempfile',    checksum_hash: 'e628c8fcb4bca3f3456640a8b56d1ae98c594e24'
   conf.gem github: 'mrbgems/mruby-yaml',        checksum_hash: '0606652a6e99d902cd3101cf2d757a7c0c37a7fd'
   conf.gem github: 'eagletmt/mruby-etc',        checksum_hash: 'v0.1.0'
