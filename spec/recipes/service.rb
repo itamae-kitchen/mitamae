ecbd7204d6963e364bce2dceffca515a1cef1dc1
Takashi Kokubun <takashikkbn@gmail.com>
2021-02-05 21:17:46 -0800

    Fix service tests for systemd

--- spec/recipes/service.rb
+++ spec/recipes/service.rb
@@ -2,12 +2,12 @@ service 'nginx' do
   action [:enable, :start]
 end
 
-execute "test -f /etc/rc3.d/S20nginx" # test
+execute "test -f /etc/systemd/system/multi-user.target.wants/nginx.service" # test
 execute "test $(ps h -C nginx | wc -l) -gt 0" # test
 
 service "nginx" do
   action [:disable, :stop]
 end
 
-execute "test ! -f /etc/rc3.d/S20nginx" # test
+execute "test ! -f /etc/systemd/system/multi-user.target.wants/nginx.service" # test
 execute "test $(ps h -C nginx | wc -l) -eq 0" # test
