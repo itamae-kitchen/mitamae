37b91fdbc111655b634f62661ef36c7c3755f3b9
sksat <sksat@sksat.net>
2021-12-31 14:11:02 +0900

    Add sensitive attribute

--- mrblib/mitamae/resource/remote_file.rb
+++ mrblib/mitamae/resource/remote_file.rb
@@ -2,6 +2,7 @@ module MItamae
   module Resource
     class RemoteFile < File
       define_attribute :source, type: [String, Symbol], default: :auto
+      define_attribute :sensitive, type: [TrueClass, FalseClass], default: false
 
       self.available_actions = [:create, :delete]
     end
--- mrblib/mitamae/resource/template.rb
+++ mrblib/mitamae/resource/template.rb
@@ -3,6 +3,7 @@ module MItamae
     class Template < RemoteFile
       attr_accessor :node
       define_attribute :variables, type: Hash, default: {}
+      define_attribute :sensitive, type: [TrueClass, FalseClass], default: false
 
       self.available_actions = [:create, :delete]
     end
