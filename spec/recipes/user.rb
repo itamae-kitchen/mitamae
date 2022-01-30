9aa70e9689bf1f2f1079ef943dbe9290072e5027
Tomoaki Yasaka <34957299+tomothy83@users.noreply.github.com>
2022-01-30 13:19:13 +0000

    Add tests of user encrypted password.

--- spec/recipes/user.rb
+++ spec/recipes/user.rb
@@ -24,3 +24,47 @@ user 'create itamae2 user with create home directory' do
   home '/home/itamae2'
   shell '/bin/sh'
 end
+
+execute 'deluser --remove-home itamae3' do
+  only_if 'id itamae3'
+end
+
+file '/tmp/itamae3-password-should-not-be-updated' do
+  action :delete
+end
+
+file '/tmp/itamae3-password-should-be-updated' do
+  action :delete
+end
+
+# salt: 'salt', password: 'password'
+itamae3_encrypted_password = '$6$salt$IxDD3jeSOb5eB1CX5LBsqZFVkJdido3OUILO5Ifz5iwMuTS4XMS130MTSuDDl3aCI6WouIL9AjRbLCelDCy.g.'
+# salt: 'salt', password: 'password2'
+itamae3_encrypted_password2 = '$6$salt$j48aGM/5GaMl0CCDSihdu1QGIFET5rK.L/ZpznS41s/HgxCe/2sNJ5kU39.gBvsagjQldZ9K8zJg0N.W9zWGp1'
+
+user 'create itamae3 user with password hash' do
+  username 'itamae3'
+  password itamae3_encrypted_password
+end
+
+user 'create itamae3 user with password hash again' do
+  username 'itamae3'
+  password itamae3_encrypted_password
+  notifies :create, 'file[itamae3 password should not be updated]'
+end
+
+user 'change password of itamae3' do
+  username 'itamae3'
+  password itamae3_encrypted_password2
+  notifies :create, 'file[itamae3 password should be updated]'
+end
+
+file 'itamae3 password should not be updated' do
+  action :nothing
+  path '/tmp/itamae3-password-should-not-be-updated'
+end
+
+file 'itamae3 password should be updated' do
+  action :nothing
+  path '/tmp/itamae3-password-should-be-updated'
+end
