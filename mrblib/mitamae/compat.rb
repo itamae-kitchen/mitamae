cdf01bceb6dee6e564ba85b925ccab72ae82f3e1
Takashi Kokubun <takashikkbn@gmail.com>
2026-04-03 17:43:45 -0700

    Fix Shellwords compatibility with mruby 3.4.0
    module_function in mruby 3.4.0 doesn't create public singleton methods,
    making Shellwords.shellescape and Shellwords.shelljoin inaccessible.
    Inline the implementation into String#shellescape and Array#shelljoin,
    and use instance method calls instead of Shellwords module methods.

--- mrblib/mitamae/compat.rb
+++ mrblib/mitamae/compat.rb
@@ -1,16 +1,22 @@
 # Compatibility patches for mruby 3.4.0
 #
-# In mruby 3.4.0, module_function + class << self alias doesn't create
-# public singleton methods properly. Redefine String#shellescape and
-# Array#shelljoin to use shellescape/shelljoin directly.
+# In mruby 3.4.0, module_function creates private singleton methods
+# instead of public ones. Work around by inlining the implementation
+# rather than delegating to Shellwords.
 class String
   def shellescape
-    Shellwords.shellescape(self)
+    str = self.to_s
+    return "''".dup if str.empty?
+
+    str = str.dup
+    str.gsub!(/([^A-Za-z0-9_\-.,:\/@\n])/, "\\\\\\1")
+    str.gsub!(/\n/, "'\n'")
+    str
   end
 end
 
 class Array
   def shelljoin
-    Shellwords.shelljoin(self)
+    self.map { |arg| arg.to_s.shellescape }.join(' ')
   end
 end
